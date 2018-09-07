7861965cb4a05f5433675376effdd81fc89c11aa
Sebastian Medina <srmedinac@unal.edu.co>
2018-09-07 17:18:32 -0500

    fix

--- app/.profile.d/ruby.sh
+++
@@ -1,8 +0,0 @@
-export LANG=${LANG:-en_US.UTF-8}
-export GEM_PATH="$HOME/vendor/bundle/ruby/2.4.0:$GEM_PATH"
-export PATH="$HOME/bin:$HOME/vendor/bundle/bin:$HOME/vendor/bundle/ruby/2.4.0/bin:$PATH"
-export RAILS_ENV=${RAILS_ENV:-production}
-export RACK_ENV=${RACK_ENV:-production}
-export SECRET_KEY_BASE=${SECRET_KEY_BASE:-64a7df3c144010201f7a6cbd1804599260944fec931e87f82bbe37e75632c49867213c686b8b959b809755d6264b8c765883813cda41854c91f751b16c0f7abc}
-export RAILS_SERVE_STATIC_FILES=${RAILS_SERVE_STATIC_FILES:-enabled}
-export RAILS_LOG_TO_STDOUT=${RAILS_LOG_TO_STDOUT:-enabled}
